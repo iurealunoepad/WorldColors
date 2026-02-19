e4c20d7b6f87cebb1720ff3f700a26ee92e37e22
Guilherme de Azevedo Palma <guilherme.azevedo.palma@gmail.com>
2026-02-19 15:49:31 +0000

    Add basic HTML structure to perguntas2.php

--- perguntas2.php
+++ perguntas2.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt_PT" dir="ltr">
 
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>WorldColors</title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
